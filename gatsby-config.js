4be6bacf72b772fc8ac9553d7f677a6f1e9ec687
Cesar Benavides <cesar@Cesars-MacBook-Pro.local>
2020-01-07 15:49:21 -0600

    First Commit to starter theme, pluigns and toher simple setup stuff.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Noisy Trumpet Starter`,
+    description: `Gatsby React Starter for Internal Development Purposes.`,
+    author: `@noisytrumpet`,
   },
   plugins: [
+    `gatsby-plugin-sitemap`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -15,6 +16,7 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-sass`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
