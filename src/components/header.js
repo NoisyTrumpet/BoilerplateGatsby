8cde95d75e7f975d3c091d818195f63b643dfce1
alexandrafossum <fossum.alexandra@gmail.com>
2020-02-21 10:55:27 -0600

    updated donate button styling; fixed spacing around quote section

--- src/components/header.js
+++ src/components/header.js
@@ -50,7 +50,8 @@ const Header = (props) => {
                 <NavLink onClick={() => scrollTo('#what-can-be-done')}>What Can <br/>Be Done?</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink className="nav-donate-btn" href="">Donate to <br/>Our Cause</NavLink>
+                {/* <NavLink className="nav-donate-btn" href="">Donate</NavLink> */}
+                <a className="nav-donate-btn" href="#">Donate</a>
               </NavItem>
             </Nav>
           </Collapse>
