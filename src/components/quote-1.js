955241705928058bc318f51ae53c626c386f8354
alexandrafossum <fossum.alexandra@gmail.com>
2020-03-03 15:22:49 -0600

    adjusting some views

--- src/components/quote-1.js
+++ src/components/quote-1.js
@@ -20,7 +20,7 @@ export default class QuoteOne extends React.Component {
             <h3>"Will your grandkids have the opportunity to fill a stock tank, swim in San Felipe Creek, marvel at the turquoise waters of the Devils River, or learn to fish on Lake Amistad?"</h3>
           </div>
         </div>
-        <div className="map-mobile d-sm-block d-md-none">
+        <div className="map-mobile d-sm-block d-md-none text-center">
           <img
             src={InternationalMap}
             alt="Map of the Edward & Trinity aquifer"
